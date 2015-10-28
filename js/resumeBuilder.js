2b24a7c37eb51b4cd60d4e4ee561e2c037962b3d
Vicente Albiter <vic.albiter@gmail.com>
2015-10-27 21:33:40 -0600

    Add bio, work, education and projects objects for resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,101 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var work = {
+ 	"jobs" : [
+ 		{
+ 			"employer" : "Procter & Gamble",
+ 			"title" : "LA Central Engineering - Intern",
+ 			"location" : "Mexico City",
+ 			"dates" : "June 2013 - December 2014",
+ 			"description" : "Internship as a tehnical leader for the Latin America Central Engineering Organization for the Homecare Business."
+ 		},
+ 		{
+ 		"employer" : "Procter & Gamble",
+ 			"title" : "Control & Information Systems Engineer",
+ 			"location" : "Mexico City",
+ 			"dates" : "January 2015 - Present",
+ 			"description" : "Regional leader of the C&IS organization for the P&G Latin America Homecare Business."
+ 		}
+ 	]
+ }
+
+ var projects = {
+ 	"projects" : [
+ 		{
+ 			"title" : "TBC",
+ 			"dates" : "TBC",
+ 			"description" : "TBC",
+ 			"images" : [
+ 				"TBC"
+ 			]
+ 		}
+ 	]
+ }
+
+ var bio = {
+ 	"name" : "Vicente Albiter",
+ 	"age" : 24,
+ 	"role" : "Web Developer",
+ 	"contacts" : {
+ 		"email" : "vic.albiter@gmail.com",
+ 		"github" : "vicalbiter",
+ 		"twitter" : "@vicalbiter",
+ 		"location" : "Mexico City"
+ 	}, 
+ 	"pictureURL" : "images/me.jpg",
+ 	"welcomeMessage" : "Welcome to my page",
+ 	"skills" : ["Programming", "Teamwork", "Leadership"]
+ };
+
+var education = {
+	"schools" : [
+	{
+		"name" : "ITESM",
+		"city" : "Toluca, Estado de Mexico",
+		"degree" : "International Baccalaurate",
+		"major" : "none"
+	},
+	{
+		"name" : "UNAM",
+		"city" : "Mexico City",
+		"degree" : "B.Eng",
+		"major" : "Mechatronics Engineering"
+	}
+	]
+}
+
+
+ /*
+
+ var awesomeThoughts = "My name is Vicente and I am AWESOME!";
+ console.log(awesomeThoughts);
+
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ console.log(funThoughts);
+
+ var name = "Vicente Albiter";
+ var role = "Web Developer"
+
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend([formattedRole]);
+ $("#header").prepend([formattedName]);
+
+ $("#main").append([work.position]);
+ $("#main").append([education.name]);
+
+ var work = {};
+ work.position = "Control Engineer";
+ work.employer = "Procter & Gamble";
+ work.years = 1;
+ work.city = "Mexico City";
+
+ var education = {};
+ education["name"] = "UNAM";
+ education["years"] = "2009 - 2014";
+ education["city"] = "Mexico City"
+*/
+
